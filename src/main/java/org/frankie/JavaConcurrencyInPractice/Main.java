2a9d6ec22372332fb5824d21fd0c580dece7f758
alefas88 <alefas88@gmail.com>
2018-05-26 18:48:43 +0300

    first commit

---
+++ src/main/java/org/frankie/JavaConcurrencyInPractice/Main.java
@@ -0,0 +1,10 @@
+package org.frankie.JavaConcurrencyInPractice;
+
+/**
+ * Created by fragkiskos on 26/05/2018.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("ok");
+    }
+}
---
+++ src/test/java/org/frankie/JavaConcurrencyInPractice/TestMain.java
@@ -0,0 +1,15 @@
+package org.frankie.JavaConcurrencyInPractice;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by fragkiskos on 26/05/2018.
+ */
+public class TestMain {
+
+    @Test
+    public void Test(){
+        Assert.assertEquals(true,true);
+    }
+}
